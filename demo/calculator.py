549b06edd7320baaa6104b8a4ff0b167d979b515
Max Tsai <maxtsai@Maxdebijixingdiannao.local>
2024-09-24 10:26:22 +0800

    first commit

---
+++ demo/calculator.py
@@ -0,0 +1,14 @@
+
+
+class Calculator:
+    def add(self, x, y) -> int:
+        return x + y
+
+    def subtract(self, x, y) -> int:
+        return x - y
+
+    def multiply(self, x, y) -> int:
+        return x * y
+    
+    def un_test(self) -> None:
+        pass
---
+++ tests/test_calculator.py
@@ -0,0 +1,28 @@
+import pytest
+
+from demo.calculator import Calculator
+
+# 初始化 Calculator 類別的測試前置作業
+@pytest.fixture
+def calculator():
+    return Calculator()
+
+def test_add(calculator):
+    assert calculator.add(1, 2) == 3
+    assert calculator.add(-1, 1) == 0
+    assert calculator.add(0, 0) == 0
+
+def test_add_type_error(calculator):
+    with pytest.raises(TypeError):
+        calculator.add(1, "a")
+
+def test_subtract(calculator):
+    assert calculator.subtract(5, 3) == 2
+    assert calculator.subtract(0, 1) == -1
+
+def test_multiply(calculator):
+    assert calculator.multiply(2, 3) == 6
+    assert calculator.multiply(0, 1) == 0
+    assert calculator.multiply(-1, 2) == -2
+
+
